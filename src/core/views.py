b3a20c6b4cae004e9cc97264d8a51fc3649134e3
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-25 23:09:52 +0100

    listmodelmixin views

--- src/core/views.py
+++ src/core/views.py
@@ -3,25 +3,32 @@ from django.http import JsonResponse
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-
+from rest_framework import generics
+from rest_framework import mixins
 from .serializers import PostSerializer
 from .models import Post
 
 # Create your views here.
 
 
-class TestView(APIView):
-    permission_classes = (IsAuthenticated,)
+class PostView(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        serializer = PostSerializer(qs, many=True)
-        return Response(serializer.data)
+        return self.list(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+# class TestView(APIView):
+#     permission_classes = (IsAuthenticated,)
+
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
-from core.views import TestView
+from core.views import PostView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("", TestView.as_view(), name="test"),
+    path("", PostView.as_view(), name="test"),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path("api/token/", obtain_auth_token, name="obatain-token"),
 ]
