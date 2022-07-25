8891fcc655d032928817c51cddb12b15fab5fea6
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-25 17:16:34 +0100

    updates

---
+++ tutorial/tutorials/quickstart/urls.py
@@ -0,0 +1,8 @@
+# from .views import *
+# from django.views.decorators.csrf import csrf_exempt
+# from django.urls import path
+
+# urlpatterns = [
+#     path("users", UserViewSet.as_view(), name="users"),
+#     path("groups", GroupViewSet.as_view(), name="groups")
+# ]
