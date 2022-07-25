ffda3884f957a8e6044e696296556e33037407f9
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-25 21:14:40 +0100

    api views

--- src/core/views.py
+++ src/core/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 # Create your views here.
 
 
-def test_view(request):
-    data = {
-        "name": "john",
-        "age": 23,
-    }
-    return JsonResponse(data)
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "name": "john",
+            "age": 23
+        }
+        return Response(data)
+
+# def test_view(request):
+#     data = {
+#         "name": "john",
+#         "age": 23,
+#     }
+#     return JsonResponse(data)
